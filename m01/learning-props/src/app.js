285688908c7de3a386bdce0a693407e6b0d7b9a9
jgecen <jgecen@gmail.com>
2021-05-08 14:56:41 -0300

    M1#A13 Conhecendo a prop key

--- m01/learning-props/src/app.js
+++ m01/learning-props/src/app.js
@@ -2,17 +2,19 @@
 
 import React, { Component } from 'react'
 import Title from './title'
+import Square from './square'
 
 class App extends Component {
   render () {
     return (
-      <div className='container'>
-        <Title name='Gecen' lastname='De Marchi' />
+      <div className="container">
+        {['blue', 'red', 'green'].map((color) => (
+          <Square key={color} color={color} />
+        ))}
       </div>
     )
   }
 }
-
 /*
 const App = React.createClass({
   render: function () {
