d0d5165284eb667e6c0f639a1906053893f77c57
jgecen <jgecen@gmail.com>
2021-05-10 12:22:05 -0300

    M1#A19 Entendendo arrow function

---
+++ m01/learning-props/src/af.js
@@ -0,0 +1,13 @@
+'use strict'
+
+console.log('mensagem..........')
+
+/*
+var sum = function(x, y){
+    return x + y;
+}
+*/
+
+var sum = (x, y) => x + y
+
+console.log(sum(1, 2))
--- m01/learning-props/src/app.js
+++ m01/learning-props/src/app.js
@@ -7,13 +7,50 @@ class App extends Component {
     super()
     this.state = { text: 'Minha app' }
   }
-
+  /*
   render() {
     return (
-         <div className='container' onClick={() => { this.setState({text: 'My App'})}} > 
-           {this.state.text}
-         </div>)
+      <div
+        className="container"
+        onClick={() => {
+          this.setState({ text: 'My App' })
+        }}
+      >
+        {this.state.text}
+      </div>
+    )
   }
+  */
+ //1 render sem arrow functions 
+ /*
+  render() {
+    const self = this;
+    return (
+      <div
+        className="container"
+        onClick={function() {
+          self.setState({ text: 'My App' })
+        }}
+      >
+        {self.state.text}
+      </div>
+    )
+  }
+  */
+ //2 formato usando bind
+  render() {
+    return (
+      <div
+        className="container"
+        onClick={function() {
+          this.setState({ text: 'My App' })
+        }.bind(this)}
+      >
+        {this.state.text}
+      </div>
+    )
+  }
+
 }
 
 export default App
