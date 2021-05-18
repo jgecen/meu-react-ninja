9488b830449357eadb67763676a2ece1f6194017
jgecen <jgecen@gmail.com>
2021-05-18 20:22:18 -0300

    M1#A22 montagem e desmontagem de componentes

--- m01/learning-state/src/app.js
+++ m01/learning-state/src/app.js
@@ -1,67 +1,29 @@
 'use strict'
 
 import React, { Component } from 'react'
-import Button from './button'
-import Square from './square'
-
-/*
-class App extends Component {
-  constructor() {
-    super()
-    this.state = { color: 'red' }
-  }
-
-  render() {
-    return (
-      <div>
-        <div>
-          <Square key='1' color={this.state.color} />
-          <Button handleClick={() => this.setState({ color: 'red' })}>
-            Vermelho
-          </Button>
-          <Button handleClick={() => this.setState({ color: 'green' })}>
-            Verde
-          </Button>
-          <Button handleClick={() => this.setState({ color: 'blue' })}>
-            Azul
-          </Button>
-        </div>
-        <div>
-        <Square key='2' color={this.state.color} />
-
-
-        </div>
-      </div>
-    )
-  }
-}
-*/
+import Timer from './timer'
 
 class App extends Component {
   constructor() {
     super()
-    console.log('constructor')
-    this.state = { color: 'red' }
+    this.state = { showTimer: true }
   }
 
-  componentWillMount(){
-    console.log('conponentWillMount')    
+  componentWillMount() {
+    console.log('conponentWillMount')
   }
 
-  componentDidMount(){
+  componentDidMount() {
     console.log('componentDidMount')
   }
 
   render() {
-    console.log('render')
     return (
       <div>
-        <Square key="1" color={this.state.color} />
-        {['red', 'blue', 'green'].map((col) => (
-          <Button key={col} handleClick={() => this.setState({ color: col })}>
-            {col}
-          </Button>
-        ))}
+        {this.state.showTimer && <Timer />}
+        <button onClick={ () =>{
+          this.setState({showTimer: !this.state.showTimer})
+        }} >show/hide</button>
       </div>
     )
   }
