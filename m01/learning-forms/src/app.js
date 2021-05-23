3365afb7a54f28621c77ae639ef4d1dfcc35399d
jgecen <jgecen@gmail.com>
2021-05-23 13:40:38 -0300

    M1#A28 Introdução a formulários

---
+++ m01/learning-forms/src/app.js
@@ -0,0 +1,22 @@
+'use strict'
+
+import React, { Component } from 'react'
+
+class App extends Component {
+  constructor(){
+    super()
+    this.state = {value: 'valor Inicial'}
+  }
+  render() {
+    return (
+      <div>
+        <form>
+          <input type="text" value={this.state.value} 
+            onChange={(e) => {this.setState({value: e.target.value})}} />
+        </form>
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ m01/learning-forms/src/title.js
@@ -0,0 +1,9 @@
+'use strict'
+
+import React from 'react'
+
+const Title = () => (
+  <h1>Hello React.js</h1>
+)
+
+export default Title
